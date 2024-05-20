4e6a114f789a75d3b5bc19b786b74b004c4b5d9a
root <root@KopekoNWan>
2024-05-20 20:22:21 +0800

    Implements CRUD and Utility methods

--- src/main/java/com/example/expense_tracker_system_ver2/DAO/ExpenseDAO.java
+++ src/main/java/com/example/expense_tracker_system_ver2/DAO/ExpenseDAO.java
@@ -18,6 +18,7 @@ public class ExpenseDAO implements ExpenseDaoInterface{
         return connection;
     }
 
+    @Override
     public void createExpense(Expense expense) {
         String sql = "INSERT INTO expenses(date, amount, category, description) VALUES(?, ?)";
         try(Connection connection = this.connection();
@@ -31,12 +32,15 @@ public class ExpenseDAO implements ExpenseDaoInterface{
         }
     }
 
+    @Override
     public List<Expense> readExpenses() {
-        String sql = "SELECT id, date, amount, category, description FROM expenses";
+        String sql = "SELECT expenses.name AS expense_name, expenses.date, expenses.amount, categories.name AS category_name " +
+                "FROM expenses " +
+                "JOIN categories ON expenses.category = categories.name";
         List<Expense> expenses = new ArrayList<>();
         try(Connection connection = this.connection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(sql)) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sql)) {
             while (resultSet.next()) {
                 Expense expense = new Expense(resultSet.getInt("id"), resultSet.getString("date"),
                         resultSet.getFloat("amount"), resultSet.getString("category"), resultSet.getString("description"));
@@ -49,20 +53,48 @@ public class ExpenseDAO implements ExpenseDaoInterface{
         return expenses;
     }
 
+
     @Override
     public void updateExpense(Expense expense) {
-
+        String sql = "UPDATE expenses SET date = ?, amount = ?, category = ?, description = ? WHERE id = ?";
+        try(PreparedStatement preparedStatement = this.connection().prepareStatement(sql)) {
+            preparedStatement.setString(1, expense.getDate());
+            preparedStatement.setFloat(2, expense.getAmount());
+            preparedStatement.setString(3, expense.getCategory());
+            preparedStatement.setString(4, expense.getDescription());
+            preparedStatement.setInt(5, expense.getId());
+            preparedStatement.executeUpdate();
+        }catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
-    public void deleteExpense(Expense expense) {
-
+    public void deleteExpense(int expenseId) {
+        String sql = "DELETE FROM expenses WHERE id = ?";
+        try(PreparedStatement preparedStatement = this.connection().prepareStatement(sql)) {
+            preparedStatement.setInt(1, expenseId);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     @Override
-    public int sumOfExpenses() {
-        return 0;
+    public float sumOfExpenses() {
+        String sql = "SELECT amount FROM expenses";
+        float totalExpense = 0f;
+        try(Connection connection = this.connection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql)) {
+            while (resultSet.next()) {
+               float expenseAmount = resultSet.getFloat("amount");
+               totalExpense += expenseAmount;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return totalExpense;
     }
-
 
 }
